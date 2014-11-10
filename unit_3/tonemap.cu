352ac712b58f03119cdb7676a46d1ef8614c458e
zogi <zoltan.gilian@gmail.com>
2014-11-10 21:34:32 +0100

    clean up code

--- unit_3/tonemap.cu
+++ unit_3/tonemap.cu
@@ -6,7 +6,7 @@
 
   A High Definition Range (HDR) image contains a wider variation of intensity
   and color than is allowed by the RGB format with 1 byte per channel that we
-  have used in the previous assignment.  
+  have used in the previous assignment.
 
   To store this extra information we use single precision floating point for
   each channel.  This allows for an extremely wide range of intensity values.
@@ -53,7 +53,7 @@
   Old TV signals used to be transmitted in this way so that black & white
   televisions could display the luminance channel while color televisions would
   display all three of the channels.
-  
+
 
   Tone-mapping
   ============
@@ -92,17 +92,17 @@ template <typename T, typename Op>
 __global__ void reduce_kernel(T const * const d_input, T * const d_output, unsigned int const n, Op const op)
 {
     assert(blockDim.x <= REDUCE_SHM);
-    
+
     unsigned int i = threadIdx.x;
     unsigned int j = threadIdx.x + blockDim.x * blockIdx.x;
 
     __shared__ T s_buf[REDUCE_SHM];
-    
+
     if (j < n)
         s_buf[i] = d_input[j];
-    
+
     __syncthreads();
-    
+
     for (unsigned int s = min(blockDim.x, n - blockDim.x * blockIdx.x); s > 1; s >>= 1) {
         T x = s_buf[i];
         if ((i & 1) == 0 && i + 1 < s)
@@ -113,7 +113,7 @@ __global__ void reduce_kernel(T const * const d_input, T * const d_output, unsig
         __syncthreads();
         s = s + (s&1);
     }
-    
+
     if (i == 0) {
         d_output[blockIdx.x] = s_buf[0];
     }
@@ -123,18 +123,18 @@ template <typename T, typename Op>
 T reduce(T const * const d_input, unsigned int const n, Op const op)
 {
     unsigned int bsz = min(512, n);
-    
+
     T * d_buf;
     T const * d_in = d_input;
     unsigned int nbuf = n / bsz + (n%bsz != 0);
     checkCudaErrors(cudaMalloc((void**)&d_buf, nbuf*sizeof(T)));
-    
+
     for (unsigned int m = n; m > 1; m = m / bsz + (m%bsz != 0)) {
         reduce_kernel<<<m/bsz + (m%bsz != 0), bsz>>>(d_in, d_buf, m, op);
         d_in = d_buf;
     }
     cudaDeviceSynchronize();
-    
+
     T res;
     checkCudaErrors(cudaMemcpy(&res, d_buf, sizeof(T), cudaMemcpyDeviceToHost));
     cudaFree(d_buf);
@@ -149,9 +149,9 @@ template <typename T>
 __global__ void histogram_kernel(T const * const d_sample, unsigned int const nsamples, T const sample_min, T const sample_max, unsigned int const nbins, unsigned int * const d_hist)
 {
     unsigned int i = threadIdx.x + blockDim.x * blockIdx.x;
-    
+
     if (i >= nsamples) return;
-    
+
     unsigned int bin = float(d_sample[i] - sample_min) / float(sample_max - sample_min) * nbins;
     bin = min(bin, nbins-1);
     atomicAdd(d_hist+bin, 1);
@@ -161,67 +161,47 @@ template <typename T, typename Op>
 __global__ void exclusive_scan_kernel(T const * const d_input, T * const d_output, T * const d_aux, unsigned int n, Op const op, T ne)
 {
     assert(blockDim.x <= SCAN_SHM);
-    
+
     unsigned int i = threadIdx.x;
     unsigned int j = threadIdx.x + blockDim.x * blockIdx.x;
 
     __shared__ T s_buf[SCAN_SHM];
-    
+
     if (j < n)
         s_buf[i] = d_input[j];
-    
+
     __syncthreads();
-    
+
     unsigned int m = min(blockDim.x, n - blockDim.x * blockIdx.x);
     if (i >= m) return;
-    
-    //if (i==0) printf("m=%d\n", m);
+
     int s;
     for (s = 1; s < m; s <<= 1) {
         int k = (i+1) % (s<<1);
         int ii = (k == 0) ? i - s : i - k + s;
-        //if (i == m-1) printf("> s: %d, i: %d, ii: %d\n", s, i, ii);
-        
+
         if (k == 0 || (i == m - 1 && ii < i))
-            s_buf[i] = op(s_buf[i], s_buf[ii]);            
-        //__syncthreads();
-        //if (k == 0 || i == m - 1)
-        //    s_buf[i] = x;
+            s_buf[i] = op(s_buf[i], s_buf[ii]);
         __syncthreads();
-        //if (i < m)
-        //    printf("%d %d\n", i, s_buf[i]);
-        //if (i == 0) printf("===\n");
     }
-    
+
     if (i == m-1) {
         if (d_aux) d_aux[blockIdx.x] = s_buf[m-1];
         s_buf[m-1] = ne;
     }
-    
-    //if (i < m)
-    //    printf("%d %d\n", i, s_buf[i]);
-    //if (i == 0) printf("===\n");
-    
+
     for (s >>= 1; s > 0; s >>= 1) {
         int k = (i+1) % (s<<1);
         int ii = (k == 0) ? i - s : i - k + s;
-        
-        //T y = x;
+
         if (k == 0 || (i == m - 1 && ii < i)) {
             T x = s_buf[i];
             s_buf[i] = op(x, s_buf[ii]);
             s_buf[ii] = x;
-        //__syncthreads();
-        //if (k == 0 || i == m - 1) {
-            //s_buf[ii] = x;
-            //s_buf[i] = y;
         }
         __syncthreads();
-        //if (i < m)
-        //    printf("%d %d\n", i, s_buf[i]);
-        //if (i == 0) printf("===\n");
     }
-    
+
     if (j < n)
          d_output[j] = s_buf[i];
 }
@@ -252,34 +232,26 @@ void exclusive_scan(T const * const d_input, T * const d_output, unsigned int co
     assert(n < 512*512);
     unsigned int bsz = min(512, n);
     unsigned int nblocks = n / bsz + (n%bsz != 0);
-    
+
     T * d_buf;
-    //T const * d_in = d_input;
     checkCudaErrors(cudaMalloc((void**)&d_buf, nblocks*sizeof(T)));
-    
+
     exclusive_scan_kernel<<<nblocks, bsz>>>(d_input, d_output, d_buf, n, op, ne);
     if (nblocks > 1) {
         exclusive_scan_kernel<<<1, nblocks>>>(d_buf, d_buf, (T*)0, n, op, ne);
         blockwise_op_kernel<<<nblocks, bsz>>>(d_output, d_buf, d_output, n, op);
     }
-    
-    //for (unsigned int m = n; m > 1; m = m / bsz + (m%bsz != 0)) {
-    //    reduce_kernel<<<m/bsz + (m%bsz != 0), bsz>>>(d_in, d_buf, m, op);
-    //    d_in = d_buf;
-    //}
+
     cudaDeviceSynchronize();
-    
-    //T res;
-    //checkCudaErrors(cudaMemcpy(&res, d_buf, sizeof(T), cudaMemcpyDeviceToHost));
+
     cudaFree(d_buf);
-    //return res;
 }
 
 template <typename T, typename Op>
 void inclusive_scan(T const * const d_input, T * const d_output, unsigned int const n, Op const op, T ne = T())
 {
     exclusive_scan(d_input, d_output, n, op);
-    
+
     unsigned int bsz = min(512, n);
     unsigned int nblocks = n / bsz + (n%bsz != 0);
     zip_kernel<<<nblocks, bsz>>>(d_output, d_input, d_output, n, op);
@@ -293,7 +265,6 @@ void your_histogram_and_prefixsum(const float* const d_logLuminance,
                                   const size_t numCols,
                                   const size_t numBins)
 {
-  //TODO
   /*Here are the steps you need to implement
     1) find the minimum and maximum value in the input logLuminance channel
        store in min_logLum and max_logLum
@@ -304,38 +275,12 @@ void your_histogram_and_prefixsum(const float* const d_logLuminance,
        the cumulative distribution of luminance values (this should go in the
        incoming d_cdf pointer which already has been allocated for you)       */
 
-    /*
-    int h_a[10000];
-    int n = 1111;
-    for (int i = 0; i < n; ++i) h_a[i] = i;
-    int * d_a, * d_b;
-    cudaMalloc((void**)&d_a, n*sizeof(int));
-    cudaMalloc((void**)&d_b, n*sizeof(int));
-    cudaMemcpy(d_a, h_a, n*sizeof(int), cudaMemcpyHostToDevice);
-    //printf("%d\n", reduce(d_a, 10000, device_plus<int>()));
-    inclusive_scan(d_a, d_b, n, device_plus<int>());
-    
-    cudaMemcpy(h_a, d_b, n*sizeof(int), cudaMemcpyDeviceToHost);
-    //for (int i = 0; i < n; ++i)
-    //    printf("%d %d %d\n", i, h_a[i], i*(i+1)/2);
-    cudaFree(d_a);
-    cudaFree(d_b);
-    
-    bool f = true;
-    for (int i = 0; i < n; ++i)
-        f = f && h_a[i] == i*(i+1)/2;
-    
-    printf("%d\n", f);
-        
-    return;
-    */
-    
-    
+
     unsigned int nsamples = numRows * numCols;
-        
+
     min_logLum = reduce(d_logLuminance, nsamples, device_min<float>());
     max_logLum = reduce(d_logLuminance, nsamples, device_max<float>());
-        
+
     unsigned int * d_hist;
     checkCudaErrors(cudaMalloc((void**)&d_hist, numBins * sizeof(unsigned int)));
 
@@ -343,21 +288,9 @@ void your_histogram_and_prefixsum(const float* const d_logLuminance,
         unsigned int bsz = min(512, nsamples);
         cudaMemset(d_hist, 0, numBins * sizeof(unsigned int));
         histogram_kernel<<<nsamples/bsz + (nsamples%bsz != 0), bsz>>>(d_logLuminance, nsamples, min_logLum, max_logLum, numBins, d_hist);
-        //int * h = new int[numBins];
-        //cudaMemcpy(d_hist, h, nbins*sizeof(int));
-        //for (int i = 0; i < nsamples; ++i)
-            
+
         inclusive_scan(d_hist, d_cdf, numBins, device_plus<int>());
-        //exclusive_scan_kernel<<<nsamples/bsz + (nsamples%bsz != 0), bsz>>>(d_hist, d_cdf, nsamples, device_plus<int>());
-        //delete[] h;
     }
-    
-    /*
-    std::vector<unsigned int> h_hist(numBins);
-    checkCudaErrors(cudaMemcpy(h_hist.data(), d_hist, numBins * sizeof(unsigned int), cudaMemcpyDeviceToHost));
-    for (int i = 0; i < numBins; ++i)
-        printf("%u\n", h_hist[i]);
-    */
-    
+
     checkCudaErrors(cudaFree(d_hist));
 }
